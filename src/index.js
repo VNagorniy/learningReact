78481fa4865a02461a26cbef26a41801b5eb0026
Doom <vnagorniyrnd@gmail.com>
2022-01-23 23:38:49 +0300

    Знакомство с ref

--- src/index.js
+++ src/index.js
@@ -4,20 +4,12 @@ import './index.css';
 import App from './App';
 import styled from 'styled-components';
 import BootstrapTest from './BootstrapTest';
-import { Button } from './App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-const BigButton = styled(Button)`
-  margin: 0 auto;
-  width: 245px;
-  text-align: center;
-`;
 
 ReactDOM.render(
   <StrictMode>
     <App />
-    <BigButton as='a'>Отправить отчёт</BigButton>
     <BootstrapTest></BootstrapTest>
   </StrictMode>,
   document.getElementById('root')
